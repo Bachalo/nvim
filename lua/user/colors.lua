d263203dfc01333ae7fcad9d08d87bd6c3026594
Michal Bastrzyk <michal.bastrzyk.pl@gmail.com>
2022-07-19 16:16:07 +0200

    Colors: disabled transparency

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,4 +1,4 @@
 vim.cmd('set termguicolors')
 -- vim.cmd('let ayucolor="dark"')
 vim.cmd('colorscheme tokyonight')
-vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
+-- vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
